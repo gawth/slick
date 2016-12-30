6017ff87e61d4c04be5f9df01fc1f3b537610de9
Alan Gawthorpe <agawthorpe@gmail.com>
2016-12-30 17:13:43 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Chat Server
+
+This application extends the code from github.com/gorilla/websocket/examples/chat
+
+Adds in a slightly nicer UI and introduces a tag to identify users in the chat.  Will gradually add in some sort of authentication, etc. 
+
+To use open http://localhost:8080/ in your browser.
---
+++ hub.go
@@ -0,0 +1,61 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+// Hub maintains the set of active clients and broadcasts messages to the
+// clients.
+type Hub struct {
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	broadcast chan []byte
+
+	// Register requests from the clients.
+	register chan *Client
+
+	// Unregister requests from clients.
+	unregister chan *Client
+
+	// History holds all previous messages - replayed to
+	// new clients
+	history [][]byte
+}
+
+func newHub() *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+			for _, msg := range h.history {
+				client.send <- msg
+			}
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			h.history = append(h.history, message)
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
